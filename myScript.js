ad606835fc87e694a898916d0179aa535f9925b4
Kenny-Hilarious <linkandnavi2014@gmail.com>
2025-03-11 11:42:20 -0400

    Link external js sheet and add button for favs

---
+++ myScript.js
@@ -0,0 +1,5 @@
+//This is where my javascript code is going to go
+//TeaCup Ride Object, What would actually help is a slide deck
+let changeFavorites = function(){
+    document.getElementById("Favorites").innerHTML = "My Favorites";
+} 

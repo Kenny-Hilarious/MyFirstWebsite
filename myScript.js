aa43736a5df7b7e5ac3f70e496f73381d6df8b8e
Kennedy Hilario <kennedyhilario03@gmail.com>
2025-03-18 21:14:51 -0400

    Khilla Blogs guts

--- myScript.js
+++ myScript.js
@@ -11,7 +11,7 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
+//Constructor
 let ride = {
     name: "The Mad Hatter",
     location: "Fantasyland",
@@ -23,17 +23,18 @@ let ride = {
 };
 console.log("Welcome to " + ride.name + "!");
 console.log("We hope you enjoy your ride!");
-
+//Method- This function allows you to interact with the teacup ride
+//prompt the user for their name to add to the guest list
 rl.question("Who will be riding with us today? ", (userName) => {
     if (userName) {
         ride.guests.push(userName);
         console.log(`${userName} has been added to ${ride.name}. Hold on tight!`);
     }
-    
+// Let's see if they can hold on!
     function rideTeacupRide(spinFactor) {
         console.log(spinFactor);
     }
-
+//Decides behavior of the ride based on user input
     function askForSpin(count) {
         if (count === 0) {
             console.log("You survived the ride, thanks for riding with us!");
@@ -63,3 +64,11 @@ rl.question("Who will be riding with us today? ", (userName) => {
     askForSpin(3);
     
 });
+//This is the constructor for the Tower of Terror ride
+let towerOfTerror = new ride(TowerOfTerror, HollywoodStudios, 180, 30, true, ["Mickey", "Goofy", "Donald"], 5);
+//you can easily add a property to an object, but not a constructor
+//This is an example of how to add a new method to an object
+towerOfTerror = function (addJohnny) {
+    towerOfTerror.guests.push("Johnny")
+}
+//^^ Might have to use the prototype method for this but you get it
